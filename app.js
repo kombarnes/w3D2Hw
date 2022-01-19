e9e53400276d6f21f2b61affcaa4bcd306110dc2
Kourtney <barnes.kourt@gmail.com>
2022-01-18 20:51:51 -0500

    push

---
+++ app.js
@@ -0,0 +1,36 @@
+function getSum(a,b) {
+
+    var sum = (a + b);
+    console.log(sum);
+
+}
+
+getSum(1,2);
+
+function getDifference(a,b) {
+
+    var difference = (a - b);
+    console.log(difference);
+
+
+}
+
+getDifference(4,2)
+
+function justMultiply(a,b) {
+
+    var multiply = (a * b);
+    console.log(multiply);
+
+}
+
+justMultiply(2,2)
+
+function justDivide(a,b) {
+
+    var divide = (a / b);
+    console.log(divide);
+
+}
+
+justDivide(10,2)
